ff6c164d76a78bee800eecb53aae5855c71695d9
kbl <m.pietraszek@gmail.com>
2011-11-03 07:29:50 +0100

    empty cell shouldnt have any neighbours

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,18 @@
 require 'rspec'
+
+class Cell
+
+  def neighbours
+    []
+  end
+
+end
+
+describe Cell do
+    
+  it 'sholuld create cell without any neighbours' do
+    cell = Cell.new
+    cell.neighbours.should be_empty
+  end
+
+end
