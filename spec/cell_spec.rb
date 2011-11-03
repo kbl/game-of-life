9c1384de0ec6818f30c6b63ce664406c0283c90f
kbl <m.pietraszek@gmail.com>
2011-11-03 07:38:54 +0100

    cell position

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,6 +2,12 @@ require 'rspec'
 
 class Cell
 
+  attr_reader :x, :y
+
+  def initialize(x = 0, y = 0)
+    @x, @y = x, y
+  end
+
   def neighbours
     []
   end
@@ -26,6 +32,25 @@ describe Cell do
   it 'sholuld create cell without any neighbours' do
     cell = Cell.new
     cell.neighbours.should be_empty
+  end
+
+  describe 'cell spawning' do
+
+    subject { Cell.new(1, 2) }
+
+    it 'should create new cell at given location x' do
+      subject.x.should == 1
+    end
+
+    it 'should create new cell at given location y' do
+      subject.y.should == 2
+    end
+
+    it 'cell created without any cords should have position 0,0' do
+      cell = Cell.new
+      cell.x.should == 0
+      cell.y.should == 0
+    end
   end
 
 end
