7de28e7e54c38bf25be72d738b8f07b18d449efd
kbl <m.pietraszek@gmail.com>
2011-11-04 11:16:06 +0100

    small refactor

--- spec/lib/gol/board_spec.rb
+++ spec/lib/gol/board_spec.rb
@@ -5,13 +5,17 @@ module Gol
 
     subject { Board.new }
 
+    def cell(*cords)
+      Cell.new(subject, *cords)
+    end
+
     describe 'board creation' do
       it 'shouldnt have any cells' do 
         subject.should be_empty
       end
       it 'should remember created cells' do
-        Cell.new(subject)
-        Cell.new(subject, 1, 3)
+        cell(0, 0)
+        cell(1, 3)
 
         subject.count.should == 2
       end
@@ -32,17 +36,17 @@ module Gol
         subject.neighbours(1, 1).should be_empty
       end
       it 'should return 2 neighbours' do
-        c1 = Cell.new(subject, 1, 0)
-        c2 = Cell.new(subject, 0, 0)
-        
-        subject.neighbours(1, 1).should contain([c1, c2])
+        c00 = cell(0, 0)
+        c10 = cell(1, 0)
+
+        subject.neighbours(1, 1).should contain([c10, c00])
       end
     end
 
     describe 'rules' do
       context 'rule 1: Any live cell with fewer than two live neighbours dies, as if caused by under-population' do
         it 'should destroy cells with neighbourhood = 0' do
-          Cell.new(subject, 1, 1)
+          cell(1, 1)
           subject.count.should == 1
 
           subject.tick
@@ -50,8 +54,8 @@ module Gol
           subject.should be_empty
         end
         it 'should destroy cells with neighbourhood = 1' do
-          Cell.new(subject, 1, 1)
-          Cell.new(subject, 1, 2)
+          cell(1, 1)
+          cell(1, 2)
           subject.count.should == 2
 
           subject.tick
@@ -61,9 +65,9 @@ module Gol
       end
       context 'rule 2: Any live cell with two or three live neighbours lives on to the next generation' do
         it 'should leave cells with neighbourhood = 2' do
-          c1 = Cell.new(subject, 1, 1)
-          c2 = Cell.new(subject, 1, 2)
-          c3 = Cell.new(subject, 2, 1)
+          c1 = cell(1, 1)
+          c2 = cell(1, 2)
+          c3 = cell(2, 1)
           subject.count.should == 3
 
           subject.tick
@@ -71,10 +75,10 @@ module Gol
           subject.should contain([c1, c2, c3])
         end
         it 'should leave cells with neighbourhood = 3' do
-          c1 = Cell.new(subject, 1, 1)
-          c2 = Cell.new(subject, 1, 2)
-          c3 = Cell.new(subject, 2, 1)
-          c4 = Cell.new(subject, 0, 0)
+          c1 = cell(1, 1)
+          c2 = cell(1, 2)
+          c3 = cell(2, 1)
+          c4 = cell(0, 0)
           subject.count.should == 4
 
           subject.tick
@@ -84,11 +88,11 @@ module Gol
       end
       context 'rule 3: Any live cell with more than three live neighbours dies, as if by overcrowding' do
         it 'should destroy cells with neighbourhood = 4' do
-          c1 = Cell.new(subject, 0, 1)
-          c2 = Cell.new(subject, 1, 1)
-          c3 = Cell.new(subject, 2, 1)
-          c4 = Cell.new(subject, 1, 0)
-          c5 = Cell.new(subject, 1, 2)
+          c1 = cell(0, 1)
+          c2 = cell(1, 1)
+          c3 = cell(2, 1)
+          c4 = cell(1, 0)
+          c5 = cell(1, 2)
           subject.count.should == 5
 
           subject.tick
@@ -97,9 +101,9 @@ module Gol
         end
         context 'rule 4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction' do
           it 'should reproduct dying cell with three neighbours' do
-            c1 = Cell.new(subject, 1, 1)
-            c2 = Cell.new(subject, 2, 1)
-            c3 = Cell.new(subject, 1, 2)
+            c1 = cell(1, 1)
+            c2 = cell(2, 1)
+            c3 = cell(1, 2)
             subject.count.should == 3
 
             subject.tick
@@ -111,10 +115,10 @@ module Gol
             c3.neighbours[0].y.should == 2
           end
           it 'should properly reproduct cells (6 from 4)' do
-            c1 = Cell.new(subject, 1, 1)
-            c2 = Cell.new(subject, 1, 2)
-            c3 = Cell.new(subject, 2, 1)
-            c4 = Cell.new(subject, 0, 0)
+            c1 = cell(1, 1)
+            c2 = cell(1, 2)
+            c3 = cell(2, 1)
+            c4 = cell(0, 0)
             subject.count.should == 4
 
             subject.tick
@@ -122,11 +126,11 @@ module Gol
             subject.count.should == 6
           end
           it 'should properly reproduct cells (8 from 5)' do
-            c1 = Cell.new(subject, 0, 1)
-            c2 = Cell.new(subject, 1, 1)
-            c3 = Cell.new(subject, 2, 1)
-            c4 = Cell.new(subject, 1, 0)
-            c5 = Cell.new(subject, 1, 2)
+            c1 = cell(0, 1)
+            c2 = cell(1, 1)
+            c3 = cell(2, 1)
+            c4 = cell(1, 0)
+            c5 = cell(1, 2)
             subject.count.should == 5
 
             subject.tick
