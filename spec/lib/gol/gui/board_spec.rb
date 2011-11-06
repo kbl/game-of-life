bab5f26869d09cedf9a753f0af869c41180b0eeb
kbl <m.pietraszek@gmail.com>
2011-11-06 17:43:28 +0100

    test for toggling field on board

--- spec/lib/gol/gui/board_spec.rb
+++ spec/lib/gol/gui/board_spec.rb
@@ -5,19 +5,43 @@ module Gol
     describe Board do
       subject { Board.new }
 
-      it 'should be possible to create board with default size 50x50' do
-        subject.x.should == 50
-        subject.y.should == 50
-      end
-      it 'should be possible to create board with chosen size' do
-        board = Board.new(Universe.new(20, 30))
+      describe 'board creation' do
+        it 'should be possible to create board with default size 50x50' do
+          subject.x.should == 50
+          subject.y.should == 50
+        end
+        it 'should be possible to create board with chosen size' do
+          board = Board.new(Universe.new(20, 30))
 
-        board.x.should == 20
-        board.y.should == 30
+          board.x.should == 20
+          board.y.should == 30
+        end
       end
-      it 'assigned shape should be packed in field instance' do
-        subject[1, 2] = 'test_shape'
-        subject[1, 2].should == 'test_shape'
+
+      describe 'field toggling' do
+        it 'should pass toggle event to universe' do
+          universe = mock('universe')
+          universe.stub(:x).and_return(10)
+          universe.stub(:y).and_return(10)
+
+          universe.should_receive(:toggle).with(1, 2)
+
+          field = stub('field')
+          field.stub(:toggle)
+
+          board = Board.new(universe)
+          board[1, 2] = field
+
+          board.toggle(1, 2)
+        end
+        it 'should pass toggle event to field' do
+          field = mock('field')
+          field.should_receive(:toggle)
+
+          subject[1, 2] = field
+
+          subject.toggle(1, 2)
+        end
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'gol/universe'
 require 'gol/gui/board'
 
 require 'rspec'
+require 'rspec/mocks'
 
 RSpec::Matchers.define :have_neighbours do |expected|
   match do |cell|
@@ -22,6 +23,9 @@ RSpec::Matchers.define :contain do |expected|
   end
 end
 
+private 
+
 def includes (universe, expected)
   expected & universe.to_a == expected
 end
+
