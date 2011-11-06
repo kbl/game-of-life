48aaa81027235e2b84eaa125306388ce77385581
kbl <m.pietraszek@gmail.com>
2011-11-06 09:54:46 +0100

    gui related code moved to new module

---
+++ lib/gol/gui/gui.rb
@@ -0,0 +1,66 @@
+$: << './../..'
+
+require 'shoes'
+require 'gol/cell'
+require 'gol/universe'
+
+module Gol
+  module Gui
+    class Board 
+      def initialize(universe, stack)
+        @universe = universe
+        @stack = stack
+      end
+
+      def repaint
+        @stack.clear
+        @stack.append do
+          @universe.x.times do |x|
+            border_x = x * 3
+            previous_blocks_x = 10 * (x + 1)
+
+            @universe.y.times do |y|
+              border_y = y * 3
+              previous_blocks_y = 10 * (y + 1)
+
+              rect(width: 10, 
+                   left: previous_blocks_x + border_x, 
+                   top: previous_blocks_y + border_y,
+                   fill: rgb(240, 240, 240),
+                   stroke: rgb(200, 200, 200))
+            end
+          end
+        end
+      end
+    end
+
+    Shoes.app(title: 'Game of life', width: 667, height: 667) do
+
+      @universe = Universe.new(50, 50)
+
+      @board = stack {
+        @universe.x.times do |x|
+          border_x = x * 3
+          previous_blocks_x = 10 * (x + 1)
+
+          @universe.y.times do |y|
+            border_y = y * 3
+            previous_blocks_y = 10 * (y + 1)
+
+            rect(width: 10, 
+                 left: previous_blocks_x + border_x, 
+                 top: previous_blocks_y + border_y,
+                 fill: rgb(240, 240, 240),
+                 stroke: rgb(200, 200, 200))
+          end
+        end
+      }
+
+      stack {
+        button 'Kroczek', {}
+        button 'Start', {}
+        button 'Stop', {}
+      }
+    end
+  end
+end
