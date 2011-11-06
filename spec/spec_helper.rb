b2a11ac5f29ab2073a528b2fe1241fc13e049295
kbl <m.pietraszek@gmail.com>
2011-11-07 00:04:26 +0100

    further passing universe specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,13 +7,15 @@ require 'rspec/mocks'
 
 RSpec::Matchers.define :have_neighbours do |expected|
   match do |actual|
+    p expected
     actual[0].cords.should == expected
   end
 end
 
 RSpec::Matchers.define :contain do |expected|
-  match do |universe|
-    includes(universe, expected)
+  match do |actual|
+    cords = actual.map { |cell| cell.cords }
+    includes(cords, expected)
   end
 end
 
