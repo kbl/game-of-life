e7970626be8f53805d6bd076cac1742c287b7194
kbl <m.pietraszek@gmail.com>
2011-11-07 00:14:49 +0100

    changes in universe spec

--- spec/lib/gol/universe_spec.rb
+++ spec/lib/gol/universe_spec.rb
@@ -195,15 +195,15 @@ module Gol
         end
         it 'should pass remove event to callback' do
           cell(1, 1)
-          callback.should_receive(:remove).with(1, 1)
+          callback.should_receive(:toggle).with(1, 1)
           subject.tick(callback)
         end
         it 'should pass 2 remove events' do
           cell(1, 1)
           cell(2, 1)
           
-          callback.should_receive(:remove).with(1, 1)
-          callback.should_receive(:remove).with(2, 1)
+          callback.should_receive(:toggle).with(1, 1)
+          callback.should_receive(:toggle).with(2, 1)
 
           subject.tick(callback)
         end
@@ -212,10 +212,10 @@ module Gol
           cell(2, 1)
           cell(3, 1)
 
-          callback.should_receive(:remove).with(1, 1)
-          callback.should_receive(:remove).with(3, 1)
-          callback.should_receive(:create).with(2, 0)
-          callback.should_receive(:create).with(2, 2)
+          callback.should_receive(:toggle).with(1, 1)
+          callback.should_receive(:toggle).with(3, 1)
+          callback.should_receive(:toggle).with(2, 0)
+          callback.should_receive(:toggle).with(2, 2)
 
           subject.tick(callback)
         end
