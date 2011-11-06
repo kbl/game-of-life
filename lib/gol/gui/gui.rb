efecefeed094c8f306f6182dd9a6beb4c27f1f14
kbl <m.pietraszek@gmail.com>
2011-11-06 10:56:11 +0100

    click event, getting cords of clicked cell

--- lib/gol/gui/gui.rb
+++ lib/gol/gui/gui.rb
@@ -4,63 +4,41 @@ require 'shoes'
 require 'gol/cell'
 require 'gol/universe'
 
+
 module Gol
   module Gui
-    class Board 
-      def initialize(universe, stack)
-        @universe = universe
-        @stack = stack
-      end
+    Shoes.app(title: 'Game of life', width: 667, height: 687) do
 
-      def repaint
-        @stack.clear
-        @stack.append do
-          @universe.x.times do |x|
-            border_x = x * 3
-            previous_blocks_x = 10 * (x + 1)
-
-            @universe.y.times do |y|
-              border_y = y * 3
-              previous_blocks_y = 10 * (y + 1)
-
-              rect(width: 10, 
-                   left: previous_blocks_x + border_x, 
-                   top: previous_blocks_y + border_y,
-                   fill: rgb(240, 240, 240),
-                   stroke: rgb(200, 200, 200))
-            end
-          end
-        end
-      end
-    end
-
-    Shoes.app(title: 'Game of life', width: 667, height: 667) do
-
+      @size = 10
+      @margin = 3
       @universe = Universe.new(50, 50)
 
-      @board = stack {
+      click { |button, x, y|
+        cord_x = (x - @size) / (@size + @margin)
+        cord_y = (y - @size) / (@size + @margin)
+
+        alert("#{cord_x} #{cord_y}")
+      }
+
+
+      @board = flow do
         @universe.x.times do |x|
-          border_x = x * 3
-          previous_blocks_x = 10 * (x + 1)
+          border_x = x * @margin
+          previous_blocks_x = @size * (x + 1)
 
           @universe.y.times do |y|
-            border_y = y * 3
-            previous_blocks_y = 10 * (y + 1)
+            border_y = y * @margin
+            previous_blocks_y = @size * (y + 1)
 
-            rect(width: 10, 
-                 left: previous_blocks_x + border_x, 
+            rect(width: @size,
+                 left: previous_blocks_x + border_x,
                  top: previous_blocks_y + border_y,
                  fill: rgb(240, 240, 240),
                  stroke: rgb(200, 200, 200))
           end
         end
-      }
-
-      stack {
-        button 'Kroczek', {}
-        button 'Start', {}
-        button 'Stop', {}
-      }
+      end
     end
   end
 end
+
